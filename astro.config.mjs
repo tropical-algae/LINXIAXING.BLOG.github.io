0a92c9acee223fccefdfc426d48f9c7c1cfa154c
LINXIAXING <15996160048@163.com>
2024-05-08 23:59:10 +0800

    [fix] Fix bug.

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://tropicalalgae.cn",
-  base: "/dist",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
